597a16278b3ff779af5060d9ce86740032e148a7
DucQuan <quanhien1999@gmail.com>
2021-05-31 08:37:11 +0700

    add First Value db

---
+++ SWP490_G9_PE/TnR_SS.Entity/Migrations/20210531013007_addFirstValue.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TnR_SS.Entity.Migrations
+{
+    public partial class addFirstValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "UserInfor",
+                type: "varchar(64)",
+                unicode: false,
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(64)",
+                oldUnicode: false,
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "RoleUser",
+                columns: new[] { "ID", "DisplayName", "RoleName" },
+                values: new object[] { 1, "Admin", "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "RoleUser",
+                columns: new[] { "ID", "DisplayName", "RoleName" },
+                values: new object[] { 2, "Trader", "Trader" });
+
+            migrationBuilder.InsertData(
+                table: "RoleUser",
+                columns: new[] { "ID", "DisplayName", "RoleName" },
+                values: new object[] { 3, "Weight Recorder", "Weight Recorder" });
+
+            migrationBuilder.InsertData(
+                table: "UserInfor",
+                columns: new[] { "ID", "Avatar", "CreatedDate", "DOB", "FirstName", "IdentifyCode", "Lastname", "Password", "PhoneNumber", "RoleID", "SaltPassword" },
+                values: new object[] { 3, null, new DateTime(2021, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1999, 10, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Admin", "123456789", "Admin", "f781bfeada73d5d4d703dca8c3b1b0eba6aa49151ac0fcfaa5d10510eaecdfd3", "admin", 1, "qwertyuiopasdfghjklz" });
+
+            migrationBuilder.InsertData(
+                table: "UserInfor",
+                columns: new[] { "ID", "Avatar", "CreatedDate", "DOB", "FirstName", "IdentifyCode", "Lastname", "Password", "PhoneNumber", "RoleID", "SaltPassword" },
+                values: new object[] { 1, null, new DateTime(2021, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1999, 10, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Nguyen", "123456789", "Quan", "1a56be4be3e34472001aa7e5f5fc5cbe84428edfe902bdf1508fcf33ff517198", "0966848112", 2, "qwertyuiopasdfghjklz" });
+
+            migrationBuilder.InsertData(
+                table: "UserInfor",
+                columns: new[] { "ID", "Avatar", "CreatedDate", "DOB", "FirstName", "IdentifyCode", "Lastname", "Password", "PhoneNumber", "RoleID", "SaltPassword" },
+                values: new object[] { 2, null, new DateTime(2021, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1999, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Anh", "123456789", "Duc", "1a56be4be3e34472001aa7e5f5fc5cbe84428edfe902bdf1508fcf33ff517198", "0969360445", 3, "qwertyuiopasdfghjklz" });
+
+            migrationBuilder.CreateIndex(
+                name: "UC_PhoneNumber",
+                table: "UserInfor",
+                columns: new[] { "ID", "PhoneNumber" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_PhoneNumber",
+                table: "UserInfor",
+                column: "PhoneNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UC_PhoneNumber",
+                table: "UserInfor");
+
+            migrationBuilder.DropIndex(
+                name: "UQ_PhoneNumber",
+                table: "UserInfor");
+
+            migrationBuilder.DeleteData(
+                table: "UserInfor",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "UserInfor",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "UserInfor",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "RoleUser",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "RoleUser",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "RoleUser",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "UserInfor",
+                type: "varchar(64)",
+                unicode: false,
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)",
+                oldUnicode: false,
+                oldMaxLength: 64);
+        }
+    }
+}
--- SWP490_G9_PE/TnR_SS.Entity/Models/TnR_SSContext.cs
+++ SWP490_G9_PE/TnR_SS.Entity/Models/TnR_SSContext.cs
@@ -24,6 +24,13 @@ namespace TnR_SS.Entity.Models
         {
             modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
 
+            var listRole = new RoleUser[]
+            {
+                new RoleUser(){ Id = 1, DisplayName = "Admin", RoleName = "Admin"},
+                new RoleUser(){ Id = 2, DisplayName = "Trader", RoleName = "Trader"},
+                new RoleUser(){ Id = 3, DisplayName = "Weight Recorder", RoleName = "Weight Recorder"},
+            };
+
             modelBuilder.Entity<RoleUser>(entity =>
             {
                 entity.ToTable("RoleUser");
@@ -38,7 +45,16 @@ namespace TnR_SS.Entity.Models
                     .IsRequired()
                     .HasMaxLength(50)
                     .IsUnicode(false);
+
+                entity.HasData(listRole);
             });
+
+            var listUser = new UserInfor[]
+            {
+                new UserInfor(){ Id = 1, Avatar = null, FirstName = "Nguyen", Lastname = "Quan", RoleId = 2, PhoneNumber = "0966848112" , CreatedDate = new DateTime(2021,5,1), Dob = new DateTime(1999,10,21), IdentifyCode = "123456789", SaltPassword = "qwertyuiopasdfghjklz", Password = "1a56be4be3e34472001aa7e5f5fc5cbe84428edfe902bdf1508fcf33ff517198"},
+                new UserInfor(){ Id = 2, Avatar = null, FirstName = "Anh", Lastname = "Duc", RoleId = 3, PhoneNumber = "0969360445" , CreatedDate = new DateTime(2021,5,1), Dob = new DateTime(1999,1,1), IdentifyCode = "123456789", SaltPassword = "qwertyuiopasdfghjklz", Password = "1a56be4be3e34472001aa7e5f5fc5cbe84428edfe902bdf1508fcf33ff517198"},
+                new UserInfor(){ Id = 3, Avatar = null, FirstName = "Admin", Lastname = "Admin", RoleId = 1, PhoneNumber = "admin" , CreatedDate = new DateTime(2021,5,1), Dob = new DateTime(1999,10,21), IdentifyCode = "123456789", SaltPassword = "qwertyuiopasdfghjklz", Password = "f781bfeada73d5d4d703dca8c3b1b0eba6aa49151ac0fcfaa5d10510eaecdfd3"},
+            };
 
             modelBuilder.Entity<UserInfor>(entity =>
             {
@@ -94,6 +110,8 @@ namespace TnR_SS.Entity.Models
                     .HasForeignKey(d => d.RoleId)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK__UserInfor__RoleUser");
+
+                entity.HasData(listUser);
             });
 
             OnModelCreatingPartial(modelBuilder);
