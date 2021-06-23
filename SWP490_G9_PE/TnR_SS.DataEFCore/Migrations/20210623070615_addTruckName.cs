0d2be3e1d7a5711b6f3d72e2f3465764e643e6a7
DucQuan <quanhien1999@gmail.com>
2021-06-23 16:40:36 +0700

    add Name to Truck

---
+++ SWP490_G9_PE/TnR_SS.DataEFCore/Migrations/20210623070615_addTruckName.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TnR_SS.DataEFCore.Migrations
+{
+    public partial class addTruckName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Truck",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "RoleUser",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "70d19cd2-2c37-4daf-97c7-ec2934b55b87");
+
+            migrationBuilder.UpdateData(
+                table: "RoleUser",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "1dd276dd-e417-4df9-aa16-6629ce4d45cb");
+
+            migrationBuilder.UpdateData(
+                table: "RoleUser",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "80c08953-2397-4ce7-b48e-91a9338adb5d");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Truck");
+
+            migrationBuilder.UpdateData(
+                table: "RoleUser",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "4598644b-0369-4aa7-826b-0c3c1f69c29e");
+
+            migrationBuilder.UpdateData(
+                table: "RoleUser",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "3f4299ab-cca2-4c89-978b-45976322bd9a");
+
+            migrationBuilder.UpdateData(
+                table: "RoleUser",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "fd76bbb2-6f9b-4744-9414-79db428dcb47");
+        }
+    }
+}
