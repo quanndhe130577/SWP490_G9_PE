83e6cb05bb303deb380c3c0c05b96ec890cf23f0
Anh Duc Ta Vu <tvad_1999@yahoo.com>
2021-08-15 22:51:10 +0700

    Sửa lại thứ tự check của chi phí phát sinh

--- SWP490_G9_PE/TnR_SS.Domain/Supervisor/TnR_SSSupervisorCostIncurred.cs
+++ SWP490_G9_PE/TnR_SS.Domain/Supervisor/TnR_SSSupervisorCostIncurred.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TnR_SS.Domain.ApiModels.CostIncurredModel;
 using TnR_SS.Domain.Entities;
@@ -23,10 +24,25 @@ namespace TnR_SS.Domain.Supervisor
 
         public async Task CreateCostIncurredAsync(CostIncurredApiModel incurred, int traderId)
         {
-            var obj = _mapper.Map<CostIncurredApiModel, CostIncurred>(incurred);
-            obj.UserId = traderId;
-            await _unitOfWork.CostIncurreds.CreateAsync(obj);
-            await _unitOfWork.SaveChangeAsync();
+            var regexCost = @"^[1-9]\d*(\.\d+)?$";
+            if (incurred.TypeOfCost == null || incurred.TypeOfCost.Trim() == "")
+            {
+                throw new Exception("Loại chi phí đang để trống!");
+            }
+            else if(incurred.Name == null || incurred.Name.Trim() == "")
+            {
+                throw new Exception("Tên chi phí không được để trống!");
+            }else if(!Regex.IsMatch(incurred.Cost.ToString(), regexCost))
+            {
+                throw new Exception("Chi phí không hợp lệ!");
+            }
+            else
+            {
+                var obj = _mapper.Map<CostIncurredApiModel, CostIncurred>(incurred);
+                obj.UserId = traderId;
+                await _unitOfWork.CostIncurreds.CreateAsync(obj);
+                await _unitOfWork.SaveChangeAsync();
+            }
         }
 
         public async Task UpdateCostIncurredAsync(CostIncurredApiModel incurred, int traderId)
