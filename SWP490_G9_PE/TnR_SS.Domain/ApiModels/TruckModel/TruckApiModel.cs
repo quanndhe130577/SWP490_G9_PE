76215801ca5326a71b7e482882c84ab9d996fb3f
Nguyễn Quân <quanhien1999@gmail.com>
2021-08-17 23:52:26 +0700

    fix error create truck api

--- SWP490_G9_PE/TnR_SS.Domain/ApiModels/TruckModel/TruckApiModel.cs
+++ SWP490_G9_PE/TnR_SS.Domain/ApiModels/TruckModel/TruckApiModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace TnR_SS.Domain.ApiModels.TruckModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [Required]
+        [MaxLength(10, ErrorMessage = "Biển số xe chỉ có tối đa 10 kí tự")]
         public string LicensePlate { get; set; }
     }
 }
